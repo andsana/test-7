c7b1ac6971f63db26dde781b44e46dfaf47f8b9a
Susanna Andreeva <sanadex90@gmail.com>
2023-11-04 15:24:06 +0900

    Добавлены элементы меню в массив MenuItems

---
+++ src/Constant/MenuItems.tsx
@@ -0,0 +1,12 @@
+interface MenuItem {
+  name: string;
+  price: number;
+}
+export const MenuItems: MenuItem[] = [
+  {name: 'Hamburger', price: 80},
+  {name: 'Cheeseburger', price: 90},
+  {name: 'Fries', price: 45},
+  {name: 'Coffee', price: 70},
+  {name: 'Tea', price: 50},
+  {name: 'Cola', price: 40},
+];
